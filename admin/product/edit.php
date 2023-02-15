42ce1baf1f5a31f4e1f829822046a198f675519f
jaldabaoth-code <grialazurabi@gmail.com>
2023-02-15 07:00:00 +0100

    php edit produit

--- admin/product/edit.php
+++ admin/product/edit.php
@@ -9,90 +9,102 @@
 ?>
 <html>
     <head>
-        <title>Consulter Categorie</title>
-    </head>
+        <?php
+            include '../../includes/head.php';
+        ?>
+		<title>Edit Produit</title>
+	</head>
     <?php
-        include("include/header.php");
-        include("include/nav.php");
+        include '../../includes/header.php';
+        include '../includes/navbar.php';
     ?>
     <body>
         <section>
-            <h3>Consulter les Categories</h3>
+            <h3>Consulter les Produits</h3>
             <?php
                 // La creation du formulaire
-                echo '<form name="categorieconsulter" action="produitModifier.php" method="POST">';
-                    // La liste deroulante
-                    $select = $bdd->query("SELECT pdt_ref, pdt_designation, pdt_prix, pdt_image, pdt_categorie FROM produit");
-                    echo '<select name="pdt_ref">';
-                        while($pdt_ref = $select->fetch()) {
-                          echo "<option>";
-                              echo $pdt_ref['pdt_ref'];
-                          echo '</option>';
-                        }
-                    echo '</select>';
-                    echo '<input type="submit" value="CONSULTER">';
-                    if (isset($_POST['pdt_ref'])) {
-                        $pdt_ref = $_POST["pdt_ref"];
+                echo '<form name="produitconsulter" action="produitModifier.php" method="POST">';
+
+                    $sql = 'SELECT pdt_ref, pdt_designation, pdt_prix, pdt_image, pdt_categorie FROM produit';
+                    $statement = $bdd->query($sql);
+                    $produits = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+                    if ($produits) {
+                        // show the publishers
+                        echo '<select name="pdt_ref">';
+                            foreach ($produits as produit) {
+                            echo "<option>";
+                                echo $produit['pdt_ref'];
+                            echo '</option>';
+                            }
+                        echo '</select>';
+                        echo '<input type="submit" value="CONSULTER">';
                     }
-                    // La creation du table
-                    $sql = "SELECT pdt_ref, pdt_designation, pdt_prix, pdt_image, pdt_categorie FROM produit WHERE pdt_ref ='$pdt_ref'";
-                    $req = $bdd->query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-                    $pdtref = "";
-                    $pdtdesignation = "";
-                    $pdtprix = "";
-                    $pdtimage = "";
-                    $pdtcategorie = "";          
-                    while ($data = mysql_fetch_assoc($req)) {
-                        $pdtref = $data['pdt_ref'];
-                        $pdtdesignation = $data['pdt_designation'];
-                        $pdtprix = $data['pdt_prix'];
-                        $pdtimage = $data['pdt_image'];
-                        $pdtcategorie = $data['pdt_categorie'];
-                    }
-                echo "</form>";
-                echo "<form name='categorieconsulter' action='produitModifier.php' method='POST'>";
-                    echo "<table align='center' name='cateajouter'>";
-                        echo "<tr align='center'>";
-                            echo "<td>Reference Produit : </td>";
-                            echo "<td><input type='text' name='pdt_ref' value='$pdtref'></td>";
-                        echo "</tr>";
-                        echo "<tr align='center'>";
-                            echo "<td>Designation Produit : </td>";
-                            echo "<td><input type='text' name='pdt_designation' value='$pdtdesignation'></td>";
-                        echo "</tr>";
-                        echo "<tr align='center'>";
-                            echo "<td>Prix Produit : </td>";
-                            echo "<td><input type='text' name='pdt_prix' value='$pdtprix'></td>";
-                        echo "</tr>";
-                        echo "<tr align='center'>";
-                            echo "<td>Image Produit : </td>";
-                            echo "<td><input type='text' name='pdt_image' value='$pdtimage'></td>";
-                        echo "</tr>";
-                        echo "<tr align='center'>";
-                            echo "<td>Categorie Produit : </td>";
-                            echo "<td><input type='text' name='pdt_categorie' value='$pdtcategorie'></td>";
-                        echo "</tr>";
-                        echo "<tr align='center'>";
-                            echo "<td colspan='2'><input type='submit' value='MODIFIER'></td>";
-                        echo "</tr>";
-                    echo "</table>";
-                echo "</form>";
-                // Insertion d'une Categorie
+                echo '</form>';
+                if (isset($_POST['pdt_ref'])) {
+                    $pdt_ref = $_POST["pdt_ref"];
+                }
+
+                $sql = 'SELECT pdt_ref, pdt_designation, pdt_prix, pdt_image, pdt_categorie FROM produit WHERE pdt_ref =:pdt_ref';
+                $statement = $bdd->query($sql);
+                $produits = $statement->fetchAll(PDO::FETCH_ASSOC);
+                // La creation du table
+
+                foreach ($produits as $produit) {
+                    $pdtref = $produit['pdt_ref'];
+                    $pdtdesignation = $produit['pdt_designation'];
+                    $pdtprix = $produit['pdt_prix'];
+                    $pdtimage = $produit['pdt_image'];
+                    $pdtcategorie = $produit['pdt_categorie'];
+
+                    echo "<form name='categorieconsulter' action='produitModifier.php' method='POST'>";
+                        echo "<table align='center' name='cateajouter'>";
+                            echo "<tr align='center'>";
+                                echo "<td>Reference Produit : </td>";
+                                echo "<td><input type='text' name='pdt_ref' value='$pdtref'></td>";
+                            echo "</tr>";
+                            echo "<tr align='center'>";
+                                echo "<td>Designation Produit : </td>";
+                                echo "<td><input type='text' name='pdt_designation' value='$pdtdesignation'></td>";
+                            echo "</tr>";
+                            echo "<tr align='center'>";
+                                echo "<td>Prix Produit : </td>";
+                                echo "<td><input type='text' name='pdt_prix' value='$pdtprix'></td>";
+                            echo "</tr>";
+                            echo "<tr align='center'>";
+                                echo "<td>Image Produit : </td>";
+                                echo "<td><input type='text' name='pdt_image' value='$pdtimage'></td>";
+                            echo "</tr>";
+                            echo "<tr align='center'>";
+                                echo "<td>Categorie Produit : </td>";
+                                echo "<td><input type='text' name='pdt_categorie' value='$pdtcategorie'></td>";
+                            echo "</tr>";
+                            echo "<tr align='center'>";
+                                echo "<td colspan='2'><input type='submit' value='MODIFIER'></td>";
+                            echo "</tr>";
+                        echo "</table>";
+                    echo "</form>";
+                }
+
+               // EDit d'un Produit
                 if (!empty($_POST['pdt_designation'])) {
                     $pdtref = $_POST["pdt_ref"];
                     $pdtdesignation = $_POST["pdt_designation"];
                     $pdtprix = $_POST["pdt_prix"];
                     $pdtimage = $_POST["pdt_image"];
                     $pdtcategorie = $_POST["pdt_categorie"];
-                    $sql = "UPDATE produit SET pdt_designation = '$pdtdesignation', pdt_prix = '$pdtprix', pdt_image = '$pdtimage', pdt_categorie = '$pdtcategorie' WHERE pdt_ref = '$pdtref'";
-                    $req = mysql_query($sql, $cnx) or die(mysql_error());
-                    if($req) {
-                      echo("La mise à jour a été correctement effectuée") ;
-                    } else {
-                      echo("La mise à jour a échouée") ;
+                    $sql = "UPDATE produit SET pdt_designation = :pdtdesignation, pdt_prix = :pdtprix, pdt_image = :pdt_image, pdt_categorie = :pdt_categorie WHERE pdt_ref = :pdt_ref";
+                    $statement = $bdd->prepare($sql);
+                    $statement->bindParam(':pdt_ref', $pdtref);
+                    $statement->bindParam(':pdtdesignation', $pdtdesignation);
+                    $statement->bindParam(':pdtprix', $pdtprix);
+                    $statement->bindParam(':pdt_image', $pdtimage);
+                    $statement->bindParam(':pdt_categorie', $pdtcategorie);
+                    // execute the UPDATE statment
+                    if ($statement->execute()) {
+                        echo 'The publisher has been updated successfully!';
                     }
                 }
-                mysql_close();
             ?>
         </section>
     </body>
