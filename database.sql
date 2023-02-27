0fb4b9189a8bbd201baebc16a3b28159fd4ee5d7
jaldabaoth-code <grialazurabi@gmail.com>
2023-02-28 00:19:46 +0100

    save

--- database.sql
+++ database.sql
@@ -11,8 +11,22 @@ USE `lafleurv2`;
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `cat_code` char(3) NOT NULL DEFAULT '',
-  `cat_libelle` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`cat_code`)
+  `id` char(3) NOT NULL DEFAULT '',
+  `name` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO category VALUES ('bul','Bulbes');
+INSERT INTO category VALUES ('mas','Plantes � massif');
+INSERT INTO category VALUES ('ros','Rosiers');
+
+CREATE TABLE product (
+  id char(3) NOT NULL DEFAULT '',
+  pdt_ref char(3) NOT NULL default '',
+  name varchar(50) NOT NULL default '',
+  pdt_prix decimal(5,2) NOT NULL default '0.00',
+  image varchar(50) NOT NULL default '',
+  category char(3) NOT NULL default '',
+  PRIMARY KEY  (pdt_ref),
+  CONSTRAINT FK_ref FOREIGN KEY (category) REFERENCES category (cat_code)
+) TYPE=InnoDB DEFAULT CHARSET=latin1;
